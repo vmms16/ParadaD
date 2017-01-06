4c0db1aa9c3b1de5b2993810a92eea28afa0da48
Vinicius Mateus <vmms16@gmail.com>
2017-01-06 09:51:29 -0200

    Initial commit

---
+++ app/src/main/java/com/example/vinicius/paradad/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.vinicius.paradad;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private int y;
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction transaction= fragmentManager.beginTransaction();
+
+        transaction.add(R.id.conteiner, new MapsFragment(),"MapsFragment");
+
+        transaction.commitAllowingStateLoss();
+    }
+}
